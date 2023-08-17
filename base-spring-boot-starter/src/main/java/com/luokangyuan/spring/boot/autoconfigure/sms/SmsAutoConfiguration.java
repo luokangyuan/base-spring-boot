c7edf53b8b2171204b58e3113ece40abb0e3b52b
luokangyuan <243518985@qq.com>
2023-08-17 21:13:21 +0800

    :sparkles: :memo: :bug: :beers: 项目初始化

---
+++ base-spring-boot-starter/src/main/java/com/luokangyuan/spring/boot/autoconfigure/sms/SmsAutoConfiguration.java
@@ -0,0 +1,12 @@
+package com.luokangyuan.spring.boot.autoconfigure.sms;
+
+/**
+ * SmsAutoConfiguration
+ *
+ * @author lky
+ * @version 1.0.0
+ * @description 短信自动配置
+ * @date 2023/8/17 21:08
+ */
+public class SmsAutoConfiguration {
+}
---
+++ base-spring-boot-starter/src/main/java/com/luokangyuan/spring/boot/autoconfigure/sms/SmsProperties.java
@@ -0,0 +1,12 @@
+package com.luokangyuan.spring.boot.autoconfigure.sms;
+
+/**
+ * SmsProperties
+ *
+ * @author lky
+ * @version 1.0.0
+ * @description 短信配置参数
+ * @date 2023/8/17 21:09
+ */
+public class SmsProperties {
+}
